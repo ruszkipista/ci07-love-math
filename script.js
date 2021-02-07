993c493f9f4effbb94e520a4325a427efb8afbbe
ruszkipista <oi5672@gmail.com>
2021-02-07 21:01:53 +0000

    add evaluateAnswer() updateCounter()

--- script.js
+++ script.js
@@ -3,43 +3,48 @@ document.addEventListener('DOMContentLoaded', function() {
     for (let button of buttons) {
         button.addEventListener('click', handleClick);
     }
-    startNewGame('addition')
+    startNewGame('+')
 });
 
+function getGameElements() {
+    let operand1 = document.getElementById('operand1');
+    let operator = document.getElementById('operator');
+    let operand2 = document.getElementById('operand2');
+    let answer = document.getElementById('answer-box');
+    return [operand1, operator, operand2, answer];
+}
+
 function startNewGame(game) {
-    let num = Math.floor(Math.random() * 25 + 1);
-    let target = document.getElementById('operand1');
-    target.textContent = num;
-    num = Math.floor(Math.random() * 25 + 1);
-    target = document.getElementById('operand2');
-    target.textContent = num;
-    target = document.getElementById('operator');
+    let gameElements = getGameElements();
+    gameElements[0].textContent = Math.floor(Math.random() * 25 + 1);
     switch (game) {
-        case 'addition':
-            target.textContent = '+';
+        case '+':
+        case '-':
+        case '*':
+        case '/':
+            gameElements[1].textContent = game;
             break;
-        case 'subtract':
-            target.textContent = '-';
-            break;
-        case 'multiply':
-            target.textContent = '*';
-            break;
-        case 'division':
-            target.textContent = '/';
-            break;
+        default:
+            gameElements[1].textContent = '?';
     }
-    target = document.getElementById('answer-box');
-    target.value = "";
+    gameElements[2].textContent = Math.floor(Math.random() * 25 + 1);
+    gameElements[3].value = "";
 }
 
 function handleClick(event) {
     let clicked = event.currentTarget.dataset.type;
     switch (clicked) {
         case 'addition':
+            startNewGame('+');
+            break;
         case 'subtract':
+            startNewGame('-');
+            break;
         case 'multiply':
+            startNewGame('*');
+            break;
         case 'division':
-            startNewGame(clicked);
+            startNewGame('/');
             break;
         case 'answer':
             evaluateAnswer();
@@ -48,5 +53,39 @@ function handleClick(event) {
 }
 
 function evaluateAnswer() {
+    let gameElements = getGameElements();
+    let num1 = parseInt(gameElements[0].textContent);
+    let operator = gameElements[1].textContent;
+    let num2 = parseInt(gameElements[2].textContent);
+    let answer = parseInt(gameElements[3].value);
+    let isCorrect = false;
+    let result = null;
+    switch (operator) {
+        case '+':
+            result = num1 + num2;
+            break;
+        case '-':
+            result = num1 - num2;
+            break;
+        case '*':
+            result = num1 * num2;
+            break;
+        case '/':
+            result = num1 / num2;
+            break;
+    }
+    if (answer === result) {
+        alert("Correct!");
+        updateCounter('counter-correct');
+    } else {
+        alert(`Wrong, correct answer is ${result}`);
+        updateCounter('counter-incorrect');
+    }
+    startNewGame(operator);
+}
 
+function updateCounter(targetCounter) {
+    let counter = document.getElementById(targetCounter);
+    let score = parseInt(counter.textContent);
+    counter.textContent = ++score;
 }
